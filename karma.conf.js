48b269019a17078d7dfaf87da86e215070ed30e1
Moobotec <daumanddavid@hotmail.fr>
2024-11-15 17:08:36 +0100

    Ajout d'un rapport de couverture de test fait par karma pour sonarqube

--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,47 @@
-// karma.conf.js
 module.exports = function(config) {
-    config.set({
-      frameworks: ['mocha', 'chai'], // Utiliser Mocha et Chai pour les tests
-      files: [
-        'node_modules/jquery/dist/jquery.min.js', // Inclure jQuery
-        'src/codeinputbuilder.js',                // Ton fichier source
-        'tests/**/*.test.js'                      // Tous les fichiers de test
-      ],
-      preprocessors: {
-        'src/codeinputbuilder.js': ['webpack'],   // Utiliser Webpack pour traiter le fichier source
-        'tests/**/*.test.js': ['webpack']         // Traiter les fichiers de test avec Webpack
-      },
-      webpack: {
-        // Config de base de Webpack
-        mode: 'development',
-        module: {
-          rules: [
-            {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env'] // Pour la compatibilité avec ES6+
-                }
+  config.set({
+    frameworks: ['mocha', 'chai'], // Utiliser Mocha et Chai pour les tests
+    files: [
+      'node_modules/jquery/dist/jquery.min.js', // Inclure jQuery
+      'src/codeinputbuilder.js',                // Ton fichier source
+      'tests/**/*.test.js'                      // Tous les fichiers de test
+    ],
+    preprocessors: {
+      'src/codeinputbuilder.js': ['webpack', 'coverage'], // Ajouter 'coverage' pour le fichier source
+      'tests/**/*.test.js': ['webpack']
+    },
+    webpack: {
+      // Config de base de Webpack
+      mode: 'development',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'] // Pour la compatibilité avec ES6+
               }
             }
-          ]
-        }
-      },
-      browsers: ['ChromeHeadless'], // Utiliser Chrome en mode headless
-      reporters: ['progress'],      // Afficher la progression des tests
-      singleRun: true,              // Arrêter après une exécution (idéal pour CI/CD)
-      plugins: [
-        'karma-mocha',
-        'karma-chai',
-        'karma-chrome-launcher',
-        'karma-webpack'
-      ]
-    });
-  };
-  
+          }
+        ]
+      }
+    },
+    browsers: ['ChromeHeadless'], // Utiliser Chrome en mode headless
+    reporters: ['progress', 'coverage'], // Ajouter 'coverage' comme reporter
+    coverageReporter: {
+      type: 'lcov',                 // Format du rapport
+      dir: 'coverage/',             // Dossier de sortie du rapport
+      subdir: '.'                   // Place le rapport dans 'coverage/lcov.info'
+    },
+    singleRun: true,                // Arrêter après une exécution (idéal pour CI/CD)
+    plugins: [
+      'karma-mocha',
+      'karma-chai',
+      'karma-chrome-launcher',
+      'karma-webpack',
+      'karma-coverage'              // Ajouter le plugin de couverture
+    ]
+  });
+};
